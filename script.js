72a4276774c66b03b26b2500ede48b0dd11b7030
thavincristally <00001079988270sp@al.educacao.sp.gov.br>
2024-09-11 15:15:11 -0300

    .

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ avanca.forEach(button => {
         const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
         atual.classList.remove('ativo');
-        document.getElementById(proximoPasso).classList.add('ativo');
+        document.getElementbyId(proximoPasso).classList.add('ativo');
     })
 })
